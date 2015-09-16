027a8c3cad0d3d7eded6cafa37b6a16044f64336
Robert Jordan <jrob698@gmail.com>
2015-09-16 14:37:43 -0400

    Added menu system

---
+++ ZeldaOracle/Game/Game/Control/Menus/MenuInventory.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using ZeldaOracle.Common.Content;
+using ZeldaOracle.Common.Geometry;
+using ZeldaOracle.Common.Graphics;
+using ZeldaOracle.Game.GameStates;
+using ZeldaOracle.Game.GameStates.Transitions;
+using ZeldaOracle.Game.Main;
+
+namespace ZeldaOracle.Game.Control.Menus {
+	public class MenuInventory : Menu {
+
+		public Menu NextMenu;
+
+		public MenuInventory(GameManager gameManager) : base() {
+			this.gameManager	= gameManager;
+			this.background = Resources.GetImage("UI/menu_weapons_b");
+			this.NextMenu = null;
+		}
+
+
+		public override void OnBegin() {
+
+		}
+
+		public override void Update(float ticks) {
+			if (Controls.Start.IsPressed()) {
+				gameManager.PopGameState();
+				//gameManager.PushGameState(new StateScreenFade(Color.White, 30, this, gameManager.RoomControl));
+				gameManager.QueueGameStates(
+					new TransitionFade(Color.White, 30, this, gameManager.RoomControl),
+					gameManager.RoomControl
+				);
+			}
+			if (Controls.Select.IsPressed()) {
+				gameManager.PopGameState();
+				gameManager.PushGameState(new MenuTransitionPush(this, NextMenu, Directions.East));
+			}
+		}
+
+		public override void Draw(Graphics2D g) {
+			g.Translate(0, 16);
+			g.DrawImage(background, Point2I.Zero);
+			g.ResetTranslation();
+			gameManager.HUD.Draw(g);
+		}
+
+	}
+}
---
+++ ZeldaOracle/Game/Game/GameStates/Transitions/TransitionFade.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using ZeldaOracle.Common.Geometry;
+using ZeldaOracle.Common.Graphics;
+
+namespace ZeldaOracle.Game.GameStates {
+	
+	public enum FadeType {
+		FadeOut,
+		FadeIn
+	}
+
+	public class TransitionFade : GameState {
+
+		// Duration in seconds.
+		private int duration;
+		// The timer for the fading.
+		private int timer;
+		// Fade in or fade out.
+		private FadeType type;
+		// Color to fade to/from.
+		private Color color;
+		// The old game state to transition from.
+		private GameState oldState;
+		// The new game state to transition to.
+		private GameState newState;
+
+
+
+		//-----------------------------------------------------------------------------
+		// Constructors
+		//-----------------------------------------------------------------------------
+
+		public TransitionFade(Color color, int duration, GameState oldState, GameState newState) :
+			base()
+		{
+			this.duration	= duration;
+			this.timer		= 0;
+			this.type		= FadeType.FadeOut;
+			this.color		= color;
+			this.oldState	= oldState;
+			this.newState	= newState;
+		}
+
+		
+		//-----------------------------------------------------------------------------
+		// Overridden methods
+		//-----------------------------------------------------------------------------
+
+		public override void OnBegin() {
+			timer = 0;
+		}
+
+		public override void Update(float timeDelta) {
+			timer++;
+			if (timer >= duration) {
+				if (type == FadeType.FadeOut) {
+					type = FadeType.FadeIn;
+					timer = 0;
+				}
+				else {
+					End();
+				}
+			}
+		}
+
+		public override void Draw(Graphics2D g) {
+			float opacity = (float)timer / (float)duration;
+			if (type == FadeType.FadeIn)
+				opacity = 1.0f - opacity;
+			Color c = color * opacity;
+			//c.A = (byte) (255.0f * opacity);
+			if (type == FadeType.FadeOut) {
+				oldState.Draw(g);
+			}
+			else {
+				newState.Draw(g);
+			}
+			g.FillRectangle(GameSettings.SCREEN_BOUNDS, c);
+		}
+	}
+}
