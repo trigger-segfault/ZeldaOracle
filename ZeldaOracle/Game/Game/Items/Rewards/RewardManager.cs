e8dcfe98b98372dc0b313a6e136da37645a5ee1d
Robert Jordan <jrob698@gmail.com>
2015-09-23 17:30:10 -0400

    Started reward manager

---
+++ ZeldaOracle/Game/Game/Items/Rewards/RewardManager.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using ZeldaOracle.Game.Control;
+
+namespace ZeldaOracle.Game.Items.Rewards {
+	public class RewardManager {
+
+		private static GameControl gameControl;
+		private static Dictionary<string, Reward> rewards;
+
+		//-----------------------------------------------------------------------------
+		// Constructors
+		//-----------------------------------------------------------------------------
+
+		public static void Initialize(GameControl gameControl) {
+			RewardManager.gameControl	= gameControl;
+			RewardManager.rewards		= new Dictionary<string, Reward>();
+		}
+	}
+}
