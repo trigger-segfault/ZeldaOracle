34a0d8ab39a21783fd240bb47bbdf23550dff074
David Jordan <jordand95@gmail.com>
2018-02-27 18:45:35 -0500

    Updated launch batch files with error checking

--- launch_editor.bat
+++ launch_editor.bat
@@ -1,5 +1,16 @@
+@echo off
+
 REM # Update the bin folder's content and binaries files
-start GameBuilder.exe -debug -out "ZeldaOracle\GameEditor\bin" -game "ZeldaOracle\Game\bin" -content "ZeldaOracle\GameContent\ZeldaContent.contentproj"
+echo GameBuilder.exe -debug -out "ZeldaOracle\ConscriptDesigner\bin" -game "ZeldaOracle\Game\bin" -editor "ZeldaOracle\GameEditor\bin" -content "ZeldaOracle\GameContent\ZeldaContent.contentproj"
+GameBuilder.exe -debug -out "ZeldaOracle\GameEditor\bin" -game "ZeldaOracle\Game\bin" -content "ZeldaOracle\GameContent\ZeldaContent.contentproj"
+
+REM # Check if there was an error
+if %ERRORLEVEL% NEQ 0 (
+  echo.
+  echo ERROR: Failure with GameBuilder.exe
+  pause
+  exit
+)
 
 REM # Launch the editor
 REM # Enter the default world path to open after ZeldaEditor.exe
